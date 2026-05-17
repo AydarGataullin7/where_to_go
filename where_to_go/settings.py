86a1fc0903ead5c2bb6d200c39efe797c1a66a43
AydarGataullin7 <aydarcr7@gmail.com>
2026-05-17 23:40:18 +0500

    Move sensitive settings to environment variables

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -1,14 +1,18 @@
 from pathlib import Path
 import os
+from environs import Env
+
+env = Env()
+env.read_env()
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-99c4a6@k@=$ewx38pywcgligvi1p3f7a9(u(d%k=8$95*4j@ez'
+SECRET_KEY = env.str('SECRET_KEY')
 
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
 
 INSTALLED_APPS = [
     'django.contrib.admin',
