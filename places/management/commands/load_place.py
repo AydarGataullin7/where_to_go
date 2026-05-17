37e7b2a43334973d8a07b3445fec846d816f003b
AydarGataullin7 <aydarcr7@gmail.com>
2026-05-18 00:24:47 +0500

    Add management command to load places from JSON

---
+++ places/management/commands/load_place.py
@@ -0,0 +1,34 @@
+import requests
+from django.core.management.base import BaseCommand
+from django.core.files.base import ContentFile
+from places.models import Place, Images
+
+
+class Command(BaseCommand):
+    help = 'Load place from JSON URL'
+
+    def add_arguments(self, parser):
+        parser.add_argument('url', help='URL of the JSON file')
+
+    def handle(self, *args, **options):
+        url = options['url']
+        response = requests.get(url)
+        data = response.json()
+
+        place, created = Place.objects.get_or_create(
+            title=data['title'],
+            defaults={
+                'description_short': data['description_short'],
+                'description_long': data['description_long'],
+                'longitude': float(data['coordinates']['lng']),
+                'latitude': float(data['coordinates']['lat']),
+            }
+        )
+
+        for img_url in data['imgs']:
+            img_response = requests.get(img_url)
+            image = Images(place=place)
+            image.image.save(img_url.split(
+                '/')[-1], ContentFile(img_response.content))
+
+        self.stdout.write(self.style.SUCCESS(f'Loaded: {place.title}'))
