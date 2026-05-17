ba42b21a960510390509bb28ebcc976726078fd3
AydarGataullin7 <aydarcr7@gmail.com>
2026-05-17 13:12:14 +0500

    Add JSON API endpoint for place details

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,6 +1,6 @@
 import json
 from django.conf import settings
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.template import loader
 from places.models import Place
 from django.shortcuts import get_object_or_404
@@ -18,7 +18,7 @@ def show_map(request):
         feature["properties"] = {}
         feature["properties"]["title"] = place.title
         feature["properties"]["placeId"] = place.id
-        feature["properties"]["detailsUrl"] = f"/place/{place.id}/"
+        feature["properties"]["detailsUrl"] = f"/places/{place.id}/"
         features.append(feature)
 
     geojson_data = {
@@ -34,4 +34,20 @@ def show_map(request):
 
 def place_detail(request, place_id):
     place = get_object_or_404(Place, pk=place_id)
-    return HttpResponse(place.title)
+    images = place.images.all().order_by('order')
+    imgs_urls = []
+    for image in images:
+        image_url = image.image.url
+        full_url = request.build_absolute_uri(image_url)
+        imgs_urls.append(full_url)
+    response_data = {
+        "title": place.title,
+        "imgs": imgs_urls,
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {
+            "lng": place.longitude,
+            "lat": place.latitude
+        }
+    }
+    return JsonResponse(response_data, json_dumps_params={'ensure_ascii': False, 'indent': 2})
