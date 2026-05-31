9e7110cbf6b6af93b008f2df366301ebbaf64996
AydarGataullin7 <aydarcr7@gmail.com>
2026-06-01 01:02:35 +0500

    Add Images to admin with raw_id_fields for performance

--- places/admin.py
+++ places/admin.py
@@ -19,6 +19,24 @@ class ImagesInline(SortableInlineAdminMixin, admin.TabularInline):
     image_preview.short_description = "Превью"
 
 
+@admin.register(Images)
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ['id', 'image_preview', 'place', 'order']
+    list_display_links = ['id', 'image_preview']
+    list_filter = ['place']
+    search_fields = ['place__title']
+    raw_id_fields = ['place']
+    readonly_fields = ['image_preview']
+    fields = ['image', 'image_preview', 'place', 'order']
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="max-height: 200px; max-width: 200px" />', obj.image.url)
+        return "Нет изображения"
+
+    image_preview.short_description = "Превью"
+
+
 @admin.register(Place)
 class PlaceAdmin(SortableAdminBase, admin.ModelAdmin):
     inlines = [ImagesInline]
