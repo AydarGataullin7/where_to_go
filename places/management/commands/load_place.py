666b9971978b71ba00a7307cfb2176cb9afb212e
AydarGataullin7 <aydarcr7@gmail.com>
2026-06-10 23:05:12 +0500

    Add HTTP status check for requests in load_place

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -13,8 +13,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         url = options['url']
-        response = requests.get(url)
-        place_data = response.json()
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            place_data = response.json()
+        except requests.exceptions.RequestException as e:
+            self.stdout.write(self.style.ERROR(f'Failed to load JSON: {e}'))
+            return
 
         place, created = Place.objects.get_or_create(
             title=place_data['title'],
@@ -24,13 +29,20 @@ class Command(BaseCommand):
                 'longitude': float(place_data['coordinates']['lng']),
                 'latitude': float(place_data['coordinates']['lat']),
             }
-
         )
 
         for img_url in place_data['imgs']:
-            img_response = requests.get(img_url)
-            image = Images(place=place)
-            image.image.save(img_url.split(
-                '/')[-1], ContentFile(img_response.content))
+            try:
+                img_response = requests.get(img_url)
+                img_response.raise_for_status()
+                image = Images(place=place)
+                image.image.save(
+                    img_url.split('/')[-1],
+                    ContentFile(img_response.content)
+                )
+            except requests.exceptions.RequestException as e:
+                self.stdout.write(self.style.ERROR(
+                    f'Failed to load image {img_url}: {e}'
+                ))
 
         self.stdout.write(self.style.SUCCESS(f'Loaded: {place.title}'))
