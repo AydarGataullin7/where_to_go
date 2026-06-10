3e886ae9903185c659f42cf234f2cb3968709806
AydarGataullin7 <aydarcr7@gmail.com>
2026-06-10 23:03:39 +0500

    Add unique constraint to Place title to prevent duplicates

--- places/models.py
+++ places/models.py
@@ -4,7 +4,8 @@ from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=200, verbose_name='Название')
+    title = models.CharField(
+        max_length=200, verbose_name='Название', unique=True)
     short_description = models.TextField(
         verbose_name='Короткое описание', blank=True)
     long_description = HTMLField(verbose_name='Полное описание', blank=True)
